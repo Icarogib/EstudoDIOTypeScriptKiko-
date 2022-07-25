a78b83d50cd1e166393edb69e2d8e1d99a10f459
Icarogib <icarogib@gmail.com>
2022-07-25 15:59:12 -0300

    Initial Commit

---
+++ Desafios/desafio1.js
@@ -0,0 +1,11 @@
+"use strict";
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+let employee = {
+    code: 10,
+    name: "John"
+};
+//ou
+let employee2 = {
+    code: 10,
+    name: "John"
+};
---
+++ Desafios/desafio1.ts
@@ -0,0 +1,13 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+let employee = {
+    code: 10,
+    name: "John"
+};
+
+//ou
+
+let employee2: {code: number, name: string} = {
+    code: 10,
+    name: "John"
+};
